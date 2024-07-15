876cce57d99b7c0a3b3735b2cc10f44d5c83217f
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:58:57 +0200

    added bordering country attributes as links

---
+++ src/components/CountryLink.tsx
@@ -0,0 +1,13 @@
+import { useCountries } from "../context/CountriesContext";
+
+const CountryLink = ({ borderCountry, index }: { borderCountry: string, index: number }) => {
+    const { countries, selectedCountry, handleSetSelectedCountry } = useCountries();
+    if (!handleSetSelectedCountry || !selectedCountry) return <></>;
+    return (
+        <h2 key={borderCountry} onClick={() => handleSetSelectedCountry(countries.findIndex(country => country.cca3 === borderCountry))}>
+            {index === selectedCountry.borders.length - 1 ? borderCountry.toUpperCase() : borderCountry.toUpperCase() + ", "}
+        </h2>
+    )
+}
+
+export default CountryLink;

37c76ab89b18ad4e0b392cc4d3b226e663b1ffdf
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:46:14 +0200

    check location of user if consent, show that country + set up skeleton for localstorage

---
+++ src/hooks.tsx/UseLocation.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const useLocation = () => {
+    const [localCountryCode, setLocalCountryCode] = useState<string | null>(null);
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            getCoordinates();
+        } else {
+            console.warn("Geolocation is not supported by this browser.");
+        }
+
+    }
+
+    const getCoordinates = () => {
+        navigator.permissions
+            .query({ name: "geolocation" })
+            .then(function (result) {
+                if (result.state === "granted") {
+                    navigator.geolocation.getCurrentPosition(getCountryCode, (error) => console.log(error));
+                }
+            });
+
+    }
+
+    const getCountryCode = (pos: GeolocationPosition) => {
+        const { latitude, longitude } = pos.coords;
+        fetch(`http://api.geonames.org/countryCodeJSON?lat=${latitude}&lng=${longitude}&username=kvtv`)
+            .then(response => response.json())
+            .then(data => {
+                setLocalCountryCode(data.countryCode);
+            })
+            .catch(error => console.log(error));
+    }
+
+    return { localCountryCode, getLocation };
+}
+
+export default useLocation;

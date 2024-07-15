7a36a707c14615865342ce6773a29ae9b8c65ff5
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:40:02 +0200

    adjusted api call and added type for country object

--- src/api/countries.tsx
+++ src/api/countries.tsx
@@ -1,6 +1,6 @@
 async function fetchCountries() {
     try {
-        const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
+        const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();
         return data;
     } catch (error) {

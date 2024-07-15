9c497b565308c0550dd0a7c3bea05337d7c533d1
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:47:22 +0200

    added autocomplete component from antd to use as selector

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -1,17 +1,29 @@
+import { AutoComplete, Input } from "antd";
 import { useCountries } from "../context/CountriesContext";
+import { Country } from "../types";
+
+const getLabel = (country: Country) =>
+    <div className="label">
+        <img className="label-flag" src={country.flags.svg} />
+        <span>{country.name.common}</span>
+    </div>
 
 const CountrySelect = () => {
     const { countries, handleSetSelectedCountry } = useCountries();
-    if (countries.length === 0) return (<div>Loading...</div>);
-    if (!handleSetSelectedCountry) return (<div>Error: setSelectedCountry is not defined</div>)
+    if (countries.length === 0) return (<div>Fetching countries...</div>);
+    if (!handleSetSelectedCountry) return (<div>Something went wrong, please try to reload the page.</div>)
     return (
-        <div>
-            <select onChange={(value) => handleSetSelectedCountry(value.target.value as unknown as number)}>
-                {countries.map((country, index) => (
-                    <option key={country.name.common} value={index}>{country.name.common}</option>
-                ))}
-            </select>
-        </div>
+        <AutoComplete
+            className="autocomplete"
+            options={countries.map((country, key) => ({ value: country.name.common, key, label: getLabel(country) }))}
+            filterOption={(inputValue, option) =>
+                option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+            }
+            onSelect={(_, option) => handleSetSelectedCountry(option.key)}
+        >
+            <Input.Search size="large" placeholder="Search for country..." />
+        </AutoComplete>
+
     )
 }
 export default CountrySelect;

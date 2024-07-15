d363f68767e4efc48f5e8c0fe3a4d1cdaecb40d1
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 13:10:55 +0200

    added space to end of each border country

--- src/components/CountryLink.tsx
+++ src/components/CountryLink.tsx
@@ -5,7 +5,7 @@ const CountryLink = ({ borderCountry, index }: { borderCountry: string, index: n
     if (!handleSetSelectedCountry || !selectedCountry) return <></>;
     return (
         <h2 key={borderCountry} onClick={() => handleSetSelectedCountry(countries.findIndex(country => country.cca3 === borderCountry))}>
-            {index === selectedCountry.borders.length - 1 ? borderCountry.toUpperCase() : borderCountry.toUpperCase() + ", "}
+            {index === selectedCountry.borders.length - 1 ? borderCountry.toUpperCase() : borderCountry.toUpperCase() + ",\u00A0"}
         </h2>
     )
 }

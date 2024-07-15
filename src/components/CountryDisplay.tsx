4746c042436e76998666e6f02c62c247bbabebb4
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:49:56 +0200

    refactored countrydisplay into countrybanner and countrybody

--- src/components/CountryDisplay.tsx
+++ src/components/CountryDisplay.tsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
 import { useCountries } from "../context/CountriesContext";
+import CountryBanner from "./CountryBanner";
+import CountryBody from "./CountryBody";
+import NoCountrySelected from "./NoCountrySelected";
 
 const CountryDisplay = () => {
     const { selectedCountry } = useCountries();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [selectedCountry])
+
+    if (!selectedCountry) return <NoCountrySelected />;
     return (
         <div>
-            <h1>
-                {selectedCountry?.name.common}
-            </h1>
-            <img src={selectedCountry?.flags.png} alt={selectedCountry?.flags.alt} />
-        </div>
+            <CountryBanner />
+            <CountryBody />
+        </div >
     )
 }
 

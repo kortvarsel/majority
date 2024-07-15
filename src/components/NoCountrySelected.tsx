016bc59bc376f8ec388cf3f7a0b884b83dc33d01
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:44:51 +0200

    created container for when no country has been selected

---
+++ src/components/NoCountrySelected.tsx
@@ -0,0 +1,13 @@
+const NoCountrySelected = () => {
+    return (
+        <div className="no-country">
+            <svg height="6rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M12 5v14M19 12l-7-7-7 7" />
+            </svg>
+            <h1>Select a country</h1>
+
+        </div>
+    )
+}
+
+export default NoCountrySelected;

d0c6f251e494704be9d79bb6558aa0283744c873
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:49:21 +0200

    refined country banner

---
+++ src/components/CountryBanner.tsx
@@ -0,0 +1,24 @@
+import { useCountries } from "../context/CountriesContext";
+import CountryName from "./CountryName";
+import CountryNameDivider from "./CountryNameDivider";
+
+const CountryBanner = () => {
+    const { selectedCountry } = useCountries();
+
+    if (!selectedCountry) return <></>;
+
+    return <div className="container-banner">
+        <div className="container-title">
+            <CountryName key="name-english" language="English" name={selectedCountry?.name.common} />
+            {Object.entries(selectedCountry?.name.nativeName ?? {}).map(([key, value]) =>
+                selectedCountry?.languages[key].toLowerCase() !== "english" && <>
+                    <CountryNameDivider />
+                    <CountryName key={`name-${selectedCountry?.languages[key]}`} language={selectedCountry?.languages[key]} name={value.common} />
+                </>
+            )}
+        </div >
+        <img className="container-flag" src={selectedCountry?.flags.png} alt={selectedCountry?.flags.alt} />
+    </div>;
+}
+
+export default CountryBanner;
---
+++ src/components/CountryNameDivider.tsx
@@ -0,0 +1,10 @@
+const CountryNameDivider = () =>
+    <div className="country-name">
+        <p style={{ color: "#222426" }}>.</p>
+        <h1>
+            <>/</>
+        </h1>
+    </div>
+
+
+export default CountryNameDivider;

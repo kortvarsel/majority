2f77620378fe5c5e32b5b82208711124eaed222b
Patric Hjorth <Patric@MacBook-Pro-4.local>
2024-07-15 12:46:14 +0200

    check location of user if consent, show that country + set up skeleton for localstorage

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,11 +1,19 @@
 import CountryDisplay from "../components/CountryDisplay";
 import CountrySelect from "../components/CountrySelect";
 import { CountriesProvider } from "../context/CountriesContext";
+import useLocation from "../hooks.tsx/UseLocation";
 
 const CountryPage = () => {
+    const savedCountry = localStorage.getItem("countryCode");
+    const { localCountryCode, getLocation } = useLocation();
+
+    if (!savedCountry) {
+        getLocation();
+    }
+
     return (
-        <CountriesProvider>
-            <div className="flex-col container-page">
+        <CountriesProvider initialCountry={localCountryCode}>
+            <div className="country-page">
                 <CountrySelect />
                 <CountryDisplay />
             </div>
